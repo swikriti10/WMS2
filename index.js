d9497df795d58af9f89eebce3b8441904c9931f7
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-06-23 06:18:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ restService.post("/wms", function (req, res) {
 
 
     const app = new App({ request: req, response: res });
-var url = "http://208.85.249.174:8000/sap/opu/odata/sap/ZWMS_BOT_SRV/";
+    var url = "http://208.85.249.174:8000/sap/opu/odata/sap/ZWMS_BOT_SRV/";
     //sess = req.session;
     var i = 0;
     var obj = [];
@@ -67,87 +67,14 @@ var url = "http://208.85.249.174:8000/sap/opu/odata/sap/ZWMS_BOT_SRV/";
     var c;
 
     //sess = req.session;
-    
-
-    if (val == "start" || val == "Start"||val=="START") {
-
-        //   sess.name ="Napo";
-         request({
-             url: url + "GetTilesSet?$filter=BotCode eq 'start'&sap-client=900&sap-language=EN&$format=json",
-           // url: url + "GetMenuSet?$filter=TileIdBot eq 'INBOUND' &sap-client=900&sap-language=EN&$format=json",
-            
-
-             //url: url + "ListOpenTOSet?$filter=UserId eq 'SAPUSER' and TorderFrom eq '' and TorderTo eq '' and DelvFrom eq '' and DelvTo eq'' and SoFrom eq '' and SoTo eq '' and Material eq '' &sap-client=900&sap-language=EN&$format=json",
-             headers: {
-                 //"Authorization": "Basic <<base64 encoded SAPUSER:crave123>>",
-                 "Authorization": "Basic c2FwdXNlcjpjcmF2ZTEyMw==",
-                 "Content-Type": "application/json",
-                 "x-csrf-token": "Fetch"
-             }
-
-         }, function (error, response, body) {
-             if (!error && response.statusCode == 200) {
-                 csrfToken = response.headers['x-csrf-token'];
-                 // console.log(csrfToken);
-                 // var gwResponse = body.asString();
-                 // var JSONObj = JSON.parse(body);
-                 var c = JSON.parse(body)
-                 //var a = res.json(body);
-                 var len = c.d.results.length;
-                 //var a = JSON.stringify(a);
 
 
-                 var obj = [];
-                 var i = 0;
-                 if (c.d.results.length > 0) {
-                     botResponse = "Choose from following menu- ";
-
-                     for (; i < c.d.results.length; i++) {
-                          botResponse += " \n";
-                        botResponse+= c.d.results[i].TileId;
-                       // botResponse+= c.d.results[i].MenuName;
-
-
-                     }
-                        
-                 }
-                 else
-                 {
-                     botResponse = "No Menu Items";
-                 }
-                    
-                 console.log(botResponse);
-                       
-             }
-
-
-             return res.json({
-                 speech: botResponse,
-                 displayText: botResponse,
-
-                 source: "webhook-echo-sample",
-
-
-         });
-
-
-
-                
-
-                   
-
-
-                });
-
-                
-    }
-
-
-    else if (selectedmenu=="INBOUND") {
+    if (val == "start" || val == "Start" || val == "START") {
 
         //   sess.name ="Napo";
         request({
-           url: url + "GetMenuSet?$filter=TileIdBot eq 'INBOUND' &sap-client=900&sap-language=EN&$format=json",
+            url: url + "GetTilesSet?$filter=BotCode eq 'start'&sap-client=900&sap-language=EN&$format=json",
+            // url: url + "GetMenuSet?$filter=TileIdBot eq 'INBOUND' &sap-client=900&sap-language=EN&$format=json",
 
 
             //url: url + "ListOpenTOSet?$filter=UserId eq 'SAPUSER' and TorderFrom eq '' and TorderTo eq '' and DelvFrom eq '' and DelvTo eq'' and SoFrom eq '' and SoTo eq '' and Material eq '' &sap-client=900&sap-language=EN&$format=json",
@@ -173,11 +100,13 @@ var url = "http://208.85.249.174:8000/sap/opu/odata/sap/ZWMS_BOT_SRV/";
                 var obj = [];
                 var i = 0;
                 if (c.d.results.length > 0) {
-                    botResponse = "Choose following options for " + selectedmenu + "- ";
+                    botResponse = "Choose from following menu- ";
 
                     for (; i < c.d.results.length; i++) {
                         botResponse += " \n";
-                        botResponse += c.d.results[i].MenuName;
+                        botResponse += c.d.results[i].TileId;
+                        // botResponse+= c.d.results[i].MenuName;
+
 
                     }
 
@@ -187,10 +116,13 @@ var url = "http://208.85.249.174:8000/sap/opu/odata/sap/ZWMS_BOT_SRV/";
                 }
 
                 console.log(botResponse);
-              
-              return res.json({
-                speech: "hi",
-                displayText: "hi",
+
+            }
+
+
+            return res.json({
+                speech: botResponse,
+                displayText: botResponse,
 
                 source: "webhook-echo-sample",
 
@@ -198,10 +130,71 @@ var url = "http://208.85.249.174:8000/sap/opu/odata/sap/ZWMS_BOT_SRV/";
             });
 
 
+        });
+
+
+    }
+
+
+    else if (selectedmenu =="INBOUND") {
+
+        //   sess.name ="Napo";
+        request({
+            url: url + "GetMenuSet?$filter=TileIdBot eq 'INBOUND' &sap-client=900&sap-language=EN&$format=json",
+
+
+            //url: url + "ListOpenTOSet?$filter=UserId eq 'SAPUSER' and TorderFrom eq '' and TorderTo eq '' and DelvFrom eq '' and DelvTo eq'' and SoFrom eq '' and SoTo eq '' and Material eq '' &sap-client=900&sap-language=EN&$format=json",
+            headers: {
+                //"Authorization": "Basic <<base64 encoded SAPUSER:crave123>>",
+                "Authorization": "Basic c2FwdXNlcjpjcmF2ZTEyMw==",
+                "Content-Type": "application/json",
+                "x-csrf-token": "Fetch"
+            }
+
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                csrfToken = response.headers['x-csrf-token'];
+                // console.log(csrfToken);
+                // var gwResponse = body.asString();
+                // var JSONObj = JSON.parse(body);
+                var c1 = JSON.parse(body)
+                //var a = res.json(body);
+                var len1 = c1.d.results.length;
+                //var a = JSON.stringify(a);
+                var botResponse1 = "";
+
+                var obj = [];
+                var i = 0;
+                if (c1.d.results.length > 0) {
+                    botResponse1 = "Choose following options for " + selectedmenu + "- ";
+
+                    for (; i < c1.d.results.length; i++) {
+                        botResponse1 += " \n";
+                        botResponse1 += c1.d.results[i].MenuName;
+
+                    }
+
+                }
+                else {
+                    botResponse1 = "No Menu Items";
+                }
+
+                //console.log(botResponse);
+
+                return res.json({
+                    speech: "botResponse1",
+                    displayText: "botResponse1",
+
+                    source: "webhook-echo-sample",
+
+
+                });
+
+
             }
 
 
-            
+
 
 
 
@@ -216,10 +209,10 @@ var url = "http://208.85.249.174:8000/sap/opu/odata/sap/ZWMS_BOT_SRV/";
 
 
 
-        });
-    
+});
 
-    
+
+
 
 
 
