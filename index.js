49d18bd3510936d160d0daec27637a47406bf15f
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-06-27 04:59:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -74,9 +74,9 @@ restService.post("/wms", function (req, res) {
 
         //   sess.name ="Napo";
         request({
-            
+
             url: url + "GetTileInfoSet?$filter=AppId%20eq%20%27WMS%27&sap-client=900&sap-language=EN&$format=json",
-           // url: url + "GetTilesSet?$filter=BotCode eq 'start'&sap-client=900&sap-language=EN&$format=json",
+            // url: url + "GetTilesSet?$filter=BotCode eq 'start'&sap-client=900&sap-language=EN&$format=json",
             // url: url + "GetMenuSet?$filter=TileIdBot eq 'INBOUND' &sap-client=900&sap-language=EN&$format=json",
 
 
@@ -140,12 +140,11 @@ restService.post("/wms", function (req, res) {
 
 
     else if (selectedmenu != "notselectedmenu" && val == "notstart" && selectedsubmenu == "notselectedsubmenu") {
-
-        
-        /////////////////Block for submenu//////////////////////////////////
         request({
-            //url: url + "GetMenuInfoSet?$filter=TileId%20eq%20%27WM_INB%27&sap-client=900&sap-language=EN&$format=json",
-            url: url + "GetMenuInfoSet?$filter=TileId%20eq%20%27WM_INB%27&sap-client=900&sap-language=EN&$format=json",
+
+            url: url + "GetTileInfoSet?$filter=AppId%20eq%20%27WMS%27&sap-client=900&sap-language=EN&$format=json",
+            // url: url + "GetTilesSet?$filter=BotCode eq 'start'&sap-client=900&sap-language=EN&$format=json",
+            // url: url + "GetMenuSet?$filter=TileIdBot eq 'INBOUND' &sap-client=900&sap-language=EN&$format=json",
 
 
             //url: url + "ListOpenTOSet?$filter=UserId eq 'SAPUSER' and TorderFrom eq '' and TorderTo eq '' and DelvFrom eq '' and DelvTo eq'' and SoFrom eq '' and SoTo eq '' and Material eq '' &sap-client=900&sap-language=EN&$format=json",
@@ -162,43 +161,109 @@ restService.post("/wms", function (req, res) {
                 // console.log(csrfToken);
                 // var gwResponse = body.asString();
                 // var JSONObj = JSON.parse(body);
-                var c1 = JSON.parse(body)
+                var c = JSON.parse(body)
                 //var a = res.json(body);
-                var len1 = c1.d.results.length;
+                var len = c.d.results.length;
                 //var a = JSON.stringify(a);
-                var botResponse1 = "";
+
 
                 var obj = [];
                 var i = 0;
-                if (c1.d.results.length > 0) {
-                    botResponse1 = "Choose following options for " + selectedmenu + "- ";
+                if (c.d.results.length > 0) {
+                    
+                    for (; i < c.d.results.length; i++) {
+                      
+                        if(c.d.results[i].TileName==selectedmenu)
+                        {
+                            var tileid = c.d.results[i].TileId
 
-                    for (; i < c1.d.results.length; i++) {
-                        botResponse1 += " \n";
-                        botResponse1 += c1.d.results[i].MenuName;
+                            /////////////////Block for submenu//////////////////////////////////
+                            request({
+                                //url: url + "GetMenuInfoSet?$filter=TileId%20eq%20%27WM_INB%27&sap-client=900&sap-language=EN&$format=json",
+                                url: url + "GetMenuInfoSet?$filter=TileId%20eq%20%27WM_INB%27&sap-client=900&sap-language=EN&$format=json",
+
+
+                                //url: url + "ListOpenTOSet?$filter=UserId eq 'SAPUSER' and TorderFrom eq '' and TorderTo eq '' and DelvFrom eq '' and DelvTo eq'' and SoFrom eq '' and SoTo eq '' and Material eq '' &sap-client=900&sap-language=EN&$format=json",
+                                headers: {
+                                    //"Authorization": "Basic <<base64 encoded SAPUSER:crave123>>",
+                                    "Authorization": "Basic c2FwdXNlcjpjcmF2ZTEyMw==",
+                                    "Content-Type": "application/json",
+                                    "x-csrf-token": "Fetch"
+                                }
+
+                            }, function (error, response, body) {
+                                if (!error && response.statusCode == 200) {
+                                    csrfToken = response.headers['x-csrf-token'];
+                                    // console.log(csrfToken);
+                                    // var gwResponse = body.asString();
+                                    // var JSONObj = JSON.parse(body);
+                                    var c1 = JSON.parse(body)
+                                    //var a = res.json(body);
+                                    var len1 = c1.d.results.length;
+                                    //var a = JSON.stringify(a);
+                                    var botResponse1 = "";
+
+                                    var obj = [];
+                                    var i = 0;
+                                    if (c1.d.results.length > 0) {
+                                        botResponse1 = "Choose following options for " + selectedmenu + "- ";
+
+                                        for (; i < c1.d.results.length; i++) {
+                                            botResponse1 += " \n";
+                                            botResponse1 += c1.d.results[i].MenuName;
+
+                                        }
+
+                                    }
+                                    else {
+                                        botResponse1 = "No Menu Items";
+                                    }
+
+                                    //console.log(botResponse);
+
+                                    return res.json({
+                                        speech: botResponse1,
+                                        displayText: botResponse1,
+
+                                        source: "webhook-echo-sample",
+
+
+                                    });
+
+
+                                }
+
+                            });
+
+
+                            ///////////////////////////////////Block for submenu/////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+                        }
+                        // botResponse+= c.d.results[i].MenuName;
+
 
                     }
 
                 }
-                else {
-                    botResponse1 = "No Menu Items";
-                }
-
-                //console.log(botResponse);
-
-                return res.json({
-                    speech: botResponse1,
-                    displayText: botResponse1,
-
-                    source: "webhook-echo-sample",
-
-
-                });
-
+                
 
             }
 
+
+            
+
         });
+        
+
+       
 
 
 
@@ -213,7 +278,7 @@ restService.post("/wms", function (req, res) {
     }
     else if (selectedsubmenu != "notselectedsubmenu" && val == "notstart" && selectedmenu == "notselectedmenu") {
         request({
-         
+
             url: url + "GetSearchFieldSet?$filter=TileId%20eq%20%27WM_INB%27%20and%20SubMenuId%20eq%20%27G_REC%27%20and%20EntitySet%20eq%20%27GRSearchSet%27&sap-client=900&sap-language=EN&$format=json",
 
 
