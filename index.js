f38d884f50c279c77c34a6e59d2435a7cfdc1ea3
swikriti10 <37859914+swikriti10@users.noreply.github.com>
2018-07-14 00:58:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -566,8 +566,8 @@ restService.post("/wms", function (req, res) {
 
             
             return res.json({
-                speech: "botResponse ",
-                displayText: "botResponse ",
+                speech: botResponse,
+                displayText: botResponse,
                 // speech: optionIntentname,
                 // displayText: optionIntentname,
                 source: "webhook-echo-sample",
